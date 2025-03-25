66d6296ce69bdbd9850e53e32cdfdce829e5265e
zap8600 <95487746+zap8600@users.noreply.github.com>
2025-03-25 12:45:19 -0500

    maybe????

--- main.c
+++ main.c
@@ -64,8 +64,8 @@ int main() {
             float xndc = vtexx/w;
             float yndc = vtexy/w;
 
-            int xp = (int)(((xndc + 1)/2)*width);
-            int yp = (int)(1-(((yndc + 1)/2)*height));
+            int xp = (int)(((xndc + 1)/2)*(width / 2));
+            int yp = (int)(1-(((yndc + 1)/2)*(height / 2)));
 
             printf("Vertex %d\nxndc: %f\nyndc: %f\nxp: %d\nyp: %d\n\n", (i / 3) + 1, xndc, yndc, xp, yp);
 
