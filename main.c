4b1fa166fabfb51e4a6a8723b068b84dbe4ae4c7
zap8600 <zapost86@gmail.com>
2025-03-31 14:13:57 -0500

    Revert "revert to working"
    This reverts commit 9a118367bffef1fa89e14f90c9348b032b55c2cd.
    
    	modified:   main.c

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
@@ -25,13 +24,6 @@ typedef struct vec3 {
     float y;
     float z;
 } vec3;
-
-typedef vec3 vtex;
-
-typedef struct face {
-    int* vtexs;
-    int vtexamt;
-} face;
 
 // Functions for manipulating 3D points
 
@@ -102,42 +94,91 @@ float dot(vec3 v1, vec3 v2) {
 
 // Functions for reading models
 
-int load_obj(const char* filename, vtex** vtexs, int* ovtexamt, face** faces, int* ofaceamt) {
+// Read an OBJ model and load it into memory
+// Texture coordinates and normal indices are read but not loaded and ignored in faces for now
+// Does not support quad faces yet
+// User has to free vertex and face arrays themselves
+int load_obj(const char* filename, vec3** vtexs, int* ovtexamt, int** faces, int* ofaceamt, vec3** tcrds, int* otcrdamt, vec3** nrmis, int* onrmiamt) {
     FILE* obj = fopen(filename, "rb");
     if(obj == NULL) {
-        fprintf(stderr, "Unable to open file %s!\n", filename);
+        fprintf(stderr, "Failed to open file %s!\n", filename);
         return 0;
     }
 
     int type;
     int vtexamt = 0;
     int faceamt = 0;
+    int tcrdamt = 0;
 
     while((type = fgetc(obj)) != EOF) {
         switch(type) {
             case 'v':
             {
-                fseek(obj, 1, SEEK_CUR);
+                type = fgetc(obj);
+                switch(type) {
+                    case ' ': {
+                        vtexamt++;
+                        (*vtexs) = (vec3*)realloc((*vtexs), vtexamt * sizeof(vec3));
 
-                vtexamt++;
-                (*vtexs) = (vtex*)realloc((*vtexs), vtexamt * sizeof(vtex));
-
-                for(int i = 0; i < 3; i++) {
-                    char buf[100];
-                    char* bufptr = buf;
-                    while(1) {
-                        int c = fgetc(obj);
-                        *bufptr++ = (char)c;
-                        if(c == ((i==2)?'\n':' ')) {
-                            *bufptr = '\0';
-                            break;
+                        char buf[100];
+                        char* bufptr = buf;
+                        for(int i = 0; i < 3; i++) {
+                            while(1) {
+                                int c = fgetc(obj);
+                                *bufptr++ = (char)c;
+                                if((c == ' ') || (c == '\n')) {
+                                    *bufptr = '\0';
+                                    break;
+                                }
+                            }
+                            float v = (float)atof(buf);
+                            switch(i) {
+                                case 0: (*vtexs)[vtexamt - 1].x = v; break;
+                                case 1: (*vtexs)[vtexamt - 1].y = v; break;
+                                case 2: (*vtexs)[vtexamt - 1].z = v; break;
+                            }
+                            bufptr = buf;
                         }
+                        break;
                     }
-                    float v = (float)atof(buf);
-                    switch(i) {
-                        case 0: (*vtexs)[vtexamt - 1].x = v; break;
-                        case 1: (*vtexs)[vtexamt - 1].y = v; break;
-                        case 2: (*vtexs)[vtexamt - 1].z = v; break;
+                    case 'n':
+                    {
+                        vtexamt++;
+                        (*vtexs) = (vec3*)realloc((*vtexs), vtexamt * sizeof(vec3));
+
+                        char buf[100];
+                        char* bufptr = buf;
+                        for(int i = 0; i < 3; i++) {
+                            while(1) {
+                                int c = fgetc(obj);
+                                *bufptr++ = (char)c;
+                                if((c == ' ') || (c == '\n')) {
+                                    *bufptr = '\0';
+                                    break;
+                                }
+                            }
+                            float v = (float)atof(buf);
+                            switch(i) {
+                                case 0: (*vtexs)[vtexamt - 1].x = v; break;
+                                case 1: (*vtexs)[vtexamt - 1].y = v; break;
+                                case 2: (*vtexs)[vtexamt - 1].z = v; break;
+                            }
+                            bufptr = buf;
+                        }
+                        break;
+                    }
+                    case 't': {
+                        int c;
+                        while((c = fgetc(obj)) != '\n') {}
+                        break;
+                    }
+                    default:
+                    {
+                        fprintf(stderr, "Unknown type: v%c\n", (char)type);
+                        fclose(obj);
+                        *ovtexamt = 0;
+                        *ofaceamt = 0;
+                        return 0;
                     }
                 }
                 break;
@@ -147,72 +188,52 @@ int load_obj(const char* filename, vtex** vtexs, int* ovtexamt, face** faces, in
                 fseek(obj, 1, SEEK_CUR);
 
                 faceamt++;
-                (*faces) = (face*)realloc((*faces), faceamt * sizeof(face));
+                (*faces) = (int*)realloc((*faces), (faceamt * 3) * sizeof(int));
 
-                int vtexfaceamt = 0;
-                bool breakl = false;
-
-                while(1) {
-                    char buf[100];
-                    char* bufptr = buf;
+                char buf[100];
+                char* bufptr = buf;
+                for(int i = 0; i < 3; i++) {
                     while(1) {
                         int c = fgetc(obj);
                         *bufptr++ = (char)c;
-                        if(c == ' ') {
-                            *bufptr = '\0';
-                            break;
-                        } else if((c == '\n') || (c == EOF)) {
-                            breakl = true;
+                        if((c == ' ') || (c == '\n') || (c == EOF)) {
                             *bufptr = '\0';
                             break;
                         }
                     }
-                    vtexfaceamt++;
-                    (*faces)[faceamt - 1].vtexs = (int*)realloc((*faces)[faceamt - 1].vtexs, vtexfaceamt * sizeof(int));
-                    int v = atoi(buf);
-                    (*faces)[faceamt - 1].vtexs[vtexfaceamt - 1] = v - 1;
-
-                    if(breakl) {
-                        (*faces)[faceamt - 1].vtexamt = vtexfaceamt;
-                        break;
-                    }
+                    int v = atoi(buf) - 1; // We need to subtract one because the obj vertex arra starts at 1 instead of 0
+                    (*faces)[((faceamt - 1) * 3) + i] = v;
+                    bufptr = buf;
                 }
-
                 break;
             }
             case '\n': continue;
             case '#':
             {
-                int c = fgetc(obj);
-                while((c != '\n') || (c != EOF)) {
-                    c = fgetc(obj);
-                }
+                int c;
+                while((c = fgetc(obj)) != '\n') {}
                 break;
             }
             default:
             {
                 fprintf(stderr, "Unknown type: %c\n", (char)type);
                 fclose(obj);
+                *ovtexamt = 0;
+                *ofaceamt = 0;
                 return 0;
             }
         }
     }
-
-    fclose(obj);
-
-    *ofaceamt = faceamt;
     *ovtexamt = vtexamt;
+    *ofaceamt = faceamt;
 
     return 1;
 }
 
-const vec3 up = {0, 1, 0};
-const vec3 realmodelpos = {0, 0, 3};
-const vec3 camerapos = {0, 0, 0};
 
-float edge(vec3 v1, vec3 v2) {
-    //
-}
+const vec3 up = {0, 1, 0};
+const vec3 realmodelpos = {0, 0, 5};
+const vec3 camerapos = {0, 0, 0};
 
 int main(int argc, char **argv) {
     if(argc != 2) {
@@ -238,12 +259,12 @@ int main(int argc, char **argv) {
     vec3* vtexs = NULL;
     int vtexamt;
 
-    face* faces = NULL;
+    int* faces = NULL;
     int faceamt;
-
-    load_obj(argv[1], &vtexs, &vtexamt, &faces, &faceamt);
     
-    vec3 modelrotpos = {0, 0, 0};
+    vec3 modelrotpos = {0, 0, 2};
+
+    load_obj(argv[1], &vtexs, &vtexamt, &faces, &faceamt); // TODO: return an error if this fails
 
     CNFGSetup("3D Renderer", (int)width, (int)height);
 
@@ -257,6 +278,9 @@ int main(int argc, char **argv) {
         float delta = ((float)(now - lasttime)) / CLOCKS_PER_SEC;
         lasttime = now;
 
+        // Rotate the object around the y axis in a circle -90 degrees per second
+        modelrotpos = roty(modelrotpos, delta*(-0.5*PI));
+
         // Rotate the model around the y axis 90 degrees per second
         // Time is tracked by measuring how long the last frame was
         // Will not rotate when the window is being moved, but will jump to the new rotation when movement stops
@@ -265,12 +289,12 @@ int main(int argc, char **argv) {
         }
 
         // Compute pixel coordinates of the points and draw lines
-        for(int i = 0; i < faceamt; i++) {
-            int* xps = (int*)malloc(faces[i].vtexamt * sizeof(int));
-            int* yps = (int*)malloc(faces[i].vtexamt * sizeof(int));
+        for(int i = 0; i < faceamt * 3; i += 3) {
+            int xps[3];
+            int yps[3];
 
-            for(int j = 0; j < faces[i].vtexamt; j++) {
-                vec3 cvtex = add(add(vtexs[faces[i].vtexs[j]], modelrotpos), realmodelpos);
+            for(int j = 0; j < 3; j++) {
+                vec3 cvtex = add(add(vtexs[faces[i + j]], modelrotpos), realmodelpos);
                 float vtexx = cvtex.x;
                 float vtexy = cvtex.y;
                 float vtexz = cvtex.z;
@@ -312,9 +336,6 @@ int main(int argc, char **argv) {
     }
 
     free(vtexs);
-    for(int i = 0; i < faceamt; i++) {
-        free(faces[i].vtexs);
-    }
     free(faces);
 
     return 0;
