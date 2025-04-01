3db295fbfaf55876b286ba0322b435397097dacd
zap8600 <95487746+zap8600@users.noreply.github.com>
2025-04-01 11:47:14 -0500

    wip spheregen write vtexs

--- spheregen.c
+++ spheregen.c
@@ -30,8 +30,36 @@ int main(int argc, char** argv) {
     // I also need to keep track of how many I have so far
     int vtexamt = 1;
 
+    // An optimization so that we don't need to calculate the top and bottom vertexs
+    vtexs[0].x = 0;
+    vtexs[0].y = 1;
+    vtexs[0].z = 0;
+
+    // Create a circle of different sizes at different Y levels, 0.5 being the biggest circle and ~1 and ~-1 being the smallest
     for(int i = 0; i < rings; i++) {
-        float p;
+        float p = PI * (((float)i + 1) / rings); // Goes from 1 to -1, used for the Y level and the size of the circle
+        for(int j = 0; j < slices; j++) {
+            float t = (2 * PI) * (((float)j) / slices); // Making the circle
+            float x = sinf(p) * cosf(t);
+            float y = cosf(p);
+            float z = sinf(p) * sinf(t);
+            vtexamt++;
+            vtexs = (vtex*)realloc(vtexs, vtexamt * sizeof(vtex));
+            vtexs[vtexamt - 1].x = x;
+            vtexs[vtexamt - 1].y = y;
+            vtexs[vtexamt - 1].z = z;
+        }
+    }
+
+    vtexamt++;
+    vtexs = (vtex*)realloc(vtexs, vtexamt * sizeof(vtex));
+    vtexs[vtexamt - 1].x = 0;
+    vtexs[vtexamt - 1].y = -1;
+    vtexs[vtexamt - 1].z = 0;
+
+    for(int i = 1; i < slices + 1; i++) {
+        int v1 = i + 1;
+        int v2 = (i + 1) % (slices + 1); // Next vertex over
     }
 
     fclose(obj);
